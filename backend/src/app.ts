aa6e7a7ae3907149ec742c55cfff3104c7f52056
mario <25mari0@protonmail.com>
2024-11-29 21:07:33 +0000

    Initial commit

---
+++ backend/src/app.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '..', '.env') });
+import AppDataSource from './db/data-source';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+// Initialize TypeORM connection
+AppDataSource.initialize()
+  .then(() => {
+    console.log('DataSource has been initialized!');
+  })
+  .catch((err) => {
+    console.error('Error during DataSource initialization:', err);
+  });
+
+
+app.get('/', (req, res) => {
+    res.send('API is running!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ backend/src/db/index.ts
@@ -0,0 +1,20 @@
+// src/db/index.ts
+import pgPromise from 'pg-promise';
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '..', '.env') });
+
+
+// Initialize pg-promise instance
+const pgp = pgPromise();
+
+// Create a database connection
+const db = pgp({
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT)
+});
+
+export default db;
